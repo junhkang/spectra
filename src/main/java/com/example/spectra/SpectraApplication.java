1cbfe46e92a8bb62165eda10d66041b76a61e5a4
junhkang <junhkang91@gmail.com>
2024-06-13 14:05:36 +0900

    feat: initial commit

---
+++ src/main/java/com/example/spectra/SpectraApplication.java
@@ -0,0 +1,13 @@
+package com.example.spectra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectraApplication.class, args);
+	}
+
+}
