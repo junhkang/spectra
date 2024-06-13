410947b0508b1bd8919985270afcbb5478ca681e
junhkang <junhkang91@gmail.com>
2024-06-13 14:05:36 +0900

    feat: initial commit

---
+++ src/test/java/com/example/spectra/SpectraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spectra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
