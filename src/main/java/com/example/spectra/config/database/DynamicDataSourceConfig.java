4abb702360ebe5dceaffe78fcbe089a1313fb55c
junhkang <junhkang91@gmail.com>
2024-06-14 12:22:19 +0900

    feat: 다이나믹 데이터소스 추가 (진행중)

--- src/main/java/com/example/spectra/config/database/DynamicDataSourceConfig.java
+++ src/main/java/com/example/spectra/config/database/DynamicDataSourceConfig.java
@@ -1,33 +1,62 @@
 package com.example.spectra.config.database;
 
-
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Getter
-@Component
+@Setter
+@Configuration
 public class DynamicDataSourceConfig {
-
     private SqlSessionTemplate dynamicSqlSessionTemplate;
 
-    public void setDynamicDataSource(String dbUrl, String dbUsername, String dbPassword, String driverClassName, String mapperLocation) throws Exception {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(dbUrl);
-        dataSource.setUsername(dbUsername);
-        dataSource.setPassword(dbPassword);
-        dataSource.setDriverClassName(driverClassName);
+    private final ApplicationContext applicationContext;
 
+    @Getter
+    @Setter
+    private static String namespace;
+
+    public DynamicDataSourceConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public void setDynamicDataSource(String dbUrl, String dbUsername, String dbPassword, String driverClassName, String namespace) throws Exception {
+        DriverManagerDataSource dataSource = createDataSource(dbUrl, dbUsername, dbPassword, driverClassName);
+        SqlSessionFactory sqlSessionFactory = createSqlSessionFactory(dataSource, namespace);
+        this.dynamicSqlSessionTemplate = new SqlSessionTemplate(Objects.requireNonNull(sqlSessionFactory));
+        updateRepositories();
+    }
+
+    private DriverManagerDataSource createDataSource(String url, String username, String password, String driverClassName) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+
+    private SqlSessionFactory createSqlSessionFactory(DataSource dataSource, String namespace) throws Exception {
         SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
         sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocation));
-
-        SqlSessionFactory sqlSessionFactory = sessionFactoryBean.getObject();
-        this.dynamicSqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/" + namespace + "/*.xml"));
+        return sessionFactoryBean.getObject();
+    }
+    private void updateRepositories() {
+        String[] beanNames = applicationContext.getBeanNamesForType(SqlSessionTemplateAware.class);
+        for (String beanName : beanNames) {
+            SqlSessionTemplateAware bean = (SqlSessionTemplateAware) applicationContext.getBean(beanName);
+            bean.setSqlSessionTemplate(this.dynamicSqlSessionTemplate);
+        }
     }
 
 }
--- src/main/java/com/example/spectra/service/DynamicDatabaseService.java
+++ src/main/java/com/example/spectra/service/DynamicDatabaseService.java
@@ -2,6 +2,7 @@ package com.example.spectra.service;
 
 import com.example.spectra.config.database.DatabaseConfig;
 import com.example.spectra.config.database.DynamicDataSourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +10,14 @@ public class DynamicDatabaseService {
 
     private final DynamicDataSourceConfig dynamicDataSourceConfig;
 
+    @Autowired
     public DynamicDatabaseService(DynamicDataSourceConfig dynamicDataSourceConfig) {
         this.dynamicDataSourceConfig = dynamicDataSourceConfig;
     }
 
     public void setDynamicDataSource(String dbUrl, String dbUsername, String dbPassword, String driverClassName, String namespace) throws Exception {
         dynamicDataSourceConfig.setDynamicDataSource(dbUrl, dbUsername, dbPassword, driverClassName, namespace);
-        DatabaseConfig.setNamespace(namespace);
+        DynamicDataSourceConfig.setNamespace("com.example.spectra.mapper." + namespace);
     }
 
 
