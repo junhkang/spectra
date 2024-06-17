73365fee640bfbe4add3892a33fde8061a0590cf
junhkang <junhkang91@gmail.com>
2024-06-17 12:29:35 +0900

    feat: test method 수정, 로그 변경

--- src/test/java/com/example/spectra/service/DynamicDatabaseServiceTest.java
+++ src/test/java/com/example/spectra/service/DynamicDatabaseServiceTest.java
@@ -3,61 +3,73 @@ package com.example.spectra.service;
 
 import com.example.spectra.config.database.DatabaseConfig;
 import com.example.spectra.config.database.DynamicDataSourceConfig;
+import com.example.spectra.repository.dynamic.IndexRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringJUnitConfig
+@SpringBootTest
+//@MybatisTest
 public class DynamicDatabaseServiceTest {
 
-    @InjectMocks
+    @Autowired
     private DynamicDatabaseService dynamicDatabaseService;
 
-    @Mock
+    @Autowired
     private DynamicDataSourceConfig dynamicDataSourceConfig;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    private IndexRepository indexRepository;
 
-    @Test
-    @DisplayName("동적 데이터 소스를 성공적으로 바인딩")
-    public void shouldSetDynamicDataSourceSuccessfully() throws Exception {
+    @BeforeEach
+    public void setUp() throws Exception {
         String dbUrl = "";
         String dbUsername = "";
         String dbPassword = "";
         String driverClassName = "";
         String namespace = "";
+        dynamicDatabaseService.setDynamicDataSource(dbUrl, dbUsername, dbPassword, driverClassName, namespace);
+    }
 
-        doNothing().when(dynamicDataSourceConfig).setDynamicDataSource(dbUrl, dbUsername, dbPassword, driverClassName, namespace);
+    @Test
+    @DisplayName("동적 데이터 소스를 성공적으로 바인딩")
+    public void shouldSetDynamicDataSourceSuccessfully() throws Exception {
+        String dbUrl = "jdbc:h2:mem:testdb";
+        String dbUsername = "sa";
+        String dbPassword = "";
+        String driverClassName = "org.h2.Driver";
+        String namespace = "testnamespace";
 
         dynamicDatabaseService.setDynamicDataSource(dbUrl, dbUsername, dbPassword, driverClassName, namespace);
-
-        verify(dynamicDataSourceConfig).setDynamicDataSource(dbUrl, dbUsername, dbPassword, driverClassName, namespace);
         assertEquals(namespace, DatabaseConfig.getNamespace());
     }
 
+    @Test
+    @DisplayName("testOne 쿼리가 올바른 값을 반환")
+    public void testGetTestOne() {
+        Integer result = indexRepository.getTestOne();
+        assertNotNull(result);
+        assertEquals(1, result);
+    }
     @Test
     @DisplayName("잘못된 데이터베이스 주소의 경우 적절한 예외처리")
     public void shouldThrowExceptionForInvalidDatabaseUrl() {
         String invalidDbUrl = "invalid_url";
         String dbUsername = "user";
         String dbPassword = "password";
-        String driverClassName = "org.postgresql.Driver";
+        String driverClassName = "org.h2.Driver";
         String namespace = "com.example.spectra.mapper";
 
         Exception exception = assertThrows(Exception.class, () -> {
             dynamicDatabaseService.setDynamicDataSource(invalidDbUrl, dbUsername, dbPassword, driverClassName, namespace);
         });
 
-        assertEquals("Invalid database URL", exception.getMessage());
+        assertTrue(exception.getMessage().contains("Invalid database URL"));
     }
 }
