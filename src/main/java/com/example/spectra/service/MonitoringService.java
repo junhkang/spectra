89aa1bcf35dd715061ef90d5052458fafc83101b
junhkang <junhkang91@gmail.com>
2024-06-18 23:11:08 +0900

    feat : monitoring repository 추가

--- src/main/java/com/example/spectra/service/MonitoringService.java
+++ src/main/java/com/example/spectra/service/MonitoringService.java
@@ -1,13 +1,18 @@
 package com.example.spectra.service;
 
 import com.example.spectra.dto.MonitoringDTO;
-import com.example.spectra.dto.SecurityDTO;
+import com.example.spectra.repository.dynamic.MonitoringRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MonitoringService {
+    private final MonitoringRepository monitoringRepository;
+
+    public MonitoringService(MonitoringRepository monitoringRepository) {
+        this.monitoringRepository = monitoringRepository;
+    }
 
     public MonitoringDTO getDatabaseStatus() {
         return new MonitoringDTO();
