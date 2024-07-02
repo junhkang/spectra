5692831f79d4e8d4be0458cbd1bfa674ee42728e
sean <siyeon0805@kakao.com>
2024-07-02 19:22:03 +0900

    Modify pom.xml to include React build process

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+          Learn
         </a>
       </header>
     </div>
